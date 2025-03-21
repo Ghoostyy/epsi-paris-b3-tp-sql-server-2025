09c4bd677157bf9f5a367edf3c629304b6f3fff2
Ghoostyy <thomas.sun@outlook.fr>
2025-03-21 15:33:09 +0100

    fix : remove useless column

--- wv_index.sql
+++ wv_index.sql
@@ -2,10 +2,6 @@
 ALTER TABLE roles 
 ADD role_type VARCHAR(50) NOT NULL DEFAULT 'undefined';
 
--- add role type in players_in_parties
-ALTER TABLE players_in_parties 
-ADD role_type VARCHAR(50) NOT NULL DEFAULT 'undefined';
-
 -- modify is_alive to a bool value in players_in_parties table
 ALTER TABLE players_in_parties
 DROP COLUMN is_alive;
